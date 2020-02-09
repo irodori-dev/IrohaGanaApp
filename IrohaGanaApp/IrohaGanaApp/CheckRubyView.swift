56f7a4db134d110b75ada1318f45f7cff67ee781
irodori <>
2020-02-09 10:32:14 +0900

    update main screen UI.

--- IrohaGanaApp/IrohaGanaApp/CheckRubyView.swift
+++ IrohaGanaApp/IrohaGanaApp/CheckRubyView.swift
@@ -27,9 +27,15 @@ class CheckRubyView: UIView {
 
     @IBOutlet weak var _inputTextView: UITextView!
 
+    @IBOutlet weak var _inputClearButton: UIButton!
+
+    @IBOutlet weak var _rubyButton: UIButton!
+
     @IBOutlet weak var _countLabel: UILabel!
-    
+
     @IBOutlet weak var _outputTextArea: UIView!
+
+    @IBOutlet weak var _outputClearButton: UIButton!
 
     @IBOutlet weak var _rubyTextView: UITextView!
     
@@ -52,6 +58,27 @@ class CheckRubyView: UIView {
         self._loadNib()
     }
     
+    
+    //MARK: -- IBAction ------------------------------
+
+    @IBAction func _tappedRubyButton(_ sender: Any) {
+        self._requestRuby(text: self._inputTextView.text)
+    }
+    
+    @IBAction func _tappedInputClearButton(_ sender: Any) {
+        self._inputTextView.text = ""
+    }
+    
+    @IBAction func _tappedOutputClearButton(_ sender: Any) {
+        self._rubyTextView.text = ""
+    }
+
+    //MARK: -- public method ------------------------------
+    
+    public func showRuby(ruby: String) {
+        self._rubyTextView.text = ruby
+    }
+    
     //MARK: -- private method ------------------------------
 
     private func _loadNib() {
@@ -70,17 +97,36 @@ class CheckRubyView: UIView {
             let image: UIImage? = UIImage(data: data)
             self._creditImage.image = image
          } catch let err {
-            print("🚨 error : \(err.localizedDescription)")
+            IrohaGanaLog.Error("\(err.localizedDescription)")
          }
         
         self._inputTextArea.layer.borderColor = IrohaGanaColor.SHINKU.cgColor
         self._inputTextArea.layer.borderWidth = 2.0
         self._inputTextArea.layer.cornerRadius = 16.0
-        self._inputTextArea.layer.masksToBounds = false
         self._inputTextArea.layer.shadowColor = UIColor.darkGray.cgColor
         self._inputTextArea.layer.shadowOpacity = 0.4
         self._inputTextArea.layer.shadowOffset = CGSize(width: 1.0, height: 1.0)
         self._inputTextArea.layer.shadowRadius = 5
+
+        self._inputClearButton.tintColor = IrohaGanaColor.SHINKU
+        self._inputClearButton.layer.borderColor = IrohaGanaColor.SHINKU.cgColor
+        self._inputClearButton.layer.borderWidth = 1.0
+        self._inputClearButton.layer.cornerRadius = 8.0
+        self._inputClearButton.contentEdgeInsets = UIEdgeInsets.init(top: 4.0, left: 4.0, bottom: 4.0, right: 4.0)
+        
+        self._rubyButton.setTitleColor(IrohaGanaColor.SHINKU, for: .normal)
+        self._rubyButton.tintColor = IrohaGanaColor.SHINKU
+        self._rubyButton.layer.borderColor = IrohaGanaColor.SHINKU.cgColor
+        self._rubyButton.layer.borderWidth = 1.0
+        self._rubyButton.layer.cornerRadius = 8.0
+        self._rubyButton.contentEdgeInsets = UIEdgeInsets.init(top: 4.0, left: 4.0, bottom: 4.0, right: 4.0)
+        
+        self._outputClearButton.tintColor = UIColor.white
+        self._outputClearButton.backgroundColor = IrohaGanaColor.SHINKU
+        self._outputClearButton.layer.borderColor = IrohaGanaColor.SHINKU.cgColor
+        self._outputClearButton.layer.borderWidth = 1.0
+        self._outputClearButton.layer.cornerRadius = 8.0
+        self._outputClearButton.contentEdgeInsets = UIEdgeInsets.init(top: 4.0, left: 4.0, bottom: 4.0, right: 4.0)
         
         self._rubyTextView.isEditable = false
         self._rubyTextView.isSelectable = false
@@ -108,14 +154,18 @@ class CheckRubyView: UIView {
 
         self._inputTextView.inputAccessoryView = kbToolBar
     }
-
-    @objc private func _doneButtonTapped() {
-        self.endEditing(true)
-        self.delegate?.requestRuby(text: self._inputTextView.text)
-    }    
     
-    public func showRuby(ruby: String) {
-        self._rubyTextView.text = ruby
+    private func _requestRuby(text: String?){
+        guard let text: String = text, text.isEmpty==false else {
+            IrohaGanaLog.Debug("no text.")
+            return
+        }
+        self.endEditing(true)
+        self.delegate?.requestRuby(text: text)
+    }
+    
+    @objc private func _doneButtonTapped() {
+        self._requestRuby(text: self._inputTextView.text)
     }
 }
 
